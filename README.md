b61aa9d544496d3a6c33342aa80184d056ceb412
Makis Otman <makisotman@gmail.com>
2014-03-16 18:27:54 +0000

    fixed indentation and updated README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 Solution(s) to the FIzzbuzz challenge with the equivalent tests.
+
+The game:
+
+- Any number divisible by 3 should return **fizz**
+- Any number divisible by 5 should return **buzz**
+- Any number divisible by both 5 and 3 should return **fizzbuzz**
+
+How to use
+----------
+
+Clone the repository with
+```
+git clone git@github.com:Maikon/FizzBuzz.git
+```
+Switch to it with
+```
+cd fizzbuzz
+```
+Then run the following command:
+```
+ruby fizzbuzz.rb
+```
+If you have rspec installed, you can also run the tests with:
+```
+rspec spec/fizzbuzz_spec.rb
+```
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,35 +1,32 @@
 class FizzBuzz
-	
-	def divisible_by(a, b)
-		a % b == 0
-	end
+  def divisible_by(a, b)
+    a % b == 0
+  end
 
-	def entry(n)
-		if divisible_by(n, 15)
-			n = "fizzbuzz"
-		elsif divisible_by(n, 3)
-			n = "fizz"
-		elsif divisible_by(n, 5)
-			n = "buzz"
-		end
-		n
-	end
+  def entry(n)
+    if divisible_by(n, 15)
+      n = 'fizzbuzz'
+    elsif divisible_by(n, 3)
+      n = 'fizz'
+    elsif divisible_by(n, 5)
+      n = 'buzz'
+    end
+    n
+  end
 
-	
-	def range
-		(1..100).map {|number| entry(number)}		
-	end
+  def range
+    (1..100).map { |number| entry(number) }
+  end
 
-	# Alternative Solution 
-	def divisible_by(n)
-		case
-			when n % 15 == 0 then n = "fizzbuzz"
-			when n % 3 == 0 then n = "fizz"
-			when n % 5 == 0 then n = "buzz"
-			else n
-		end
-	end
-
+# Alternative Solution
+  # def divisible_by(n)
+  #   case
+  #   when n % 15 == 0 then n = 'fizzbuzz'
+  #   when n % 3 == 0 then n = 'fizz'
+  #   when n % 5 == 0 then n = 'buzz'
+  #   else n
+  #   end
+  # end
 end
 
 puts FizzBuzz.new.range
